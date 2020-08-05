b34428458ee2dbb2832638983bb95ea72d16641d
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-05 01:04:25 -0500

    Update Program.cs

--- BadBits.Engine/Program.cs
+++ BadBits.Engine/Program.cs
@@ -6,6 +6,26 @@ using System.Windows.Forms;
 
 namespace BadBits.Engine
 {
+    /// <summary>
+    /// OK - Notes
+    /// 
+    /// Step 1 - make sure the file is real and it's there. If it's not there, then bail.
+    /// 
+    /// Step 2 - initialize the engine. 
+    ///     Create the window with a loading screen
+    ///     setup the open gl settings
+    ///     setup the bindings
+    ///     
+    /// Step 3 - invoke the entrypoint script.
+    /// 
+    /// the script should call register() with an object that looks like this
+    ///     { onRender : (dt, ctx)=> void,
+    ///         onFrame: (dt) => void,
+    ///         onClose: ()=> void,
+    ///         onInit: ()=> void}
+    ///         
+    /// </summary>
+
     static class Program
     {
         /// <summary>
@@ -15,6 +35,8 @@ namespace BadBits.Engine
         static void Main()
         {
             using (var window = new OpenTK.GameWindow()) {
+
+                
 
                 window.Run();
 
