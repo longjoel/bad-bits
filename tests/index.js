f210a415091c791cf7e1218099786932d4d1599a
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-05 23:02:23 -0500

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,15 @@
 "use strict";
 
 badBits = badBits || {};
+
+let x = 0;
+
+const clamp = function (x) {
+
+    return x < 1000 ? x : 1000;
+}
+
 badBits.SetRender(function (dt) {
-    badBits.Info('1');
+    x = x + dt;
+    badBits.Info(clamp(x));
 });
