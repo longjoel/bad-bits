e93504285c7385fbe4a2c3a998493afd355b9583
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-09 13:11:56 -0500

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -2,13 +2,11 @@
 
 const backup = require('./backup.js');
 
-badBits = badBits || {};
-
 badBits.setRender2d(function (dt, g) {
 
     g.clear();
 
-    g.setRect(0, 0, 16, 16, 0, 0, 0, 0xFF);
+    g.setRect(0, 0, 16, 16, 255, 255, 255, 0xFF);
     g.setRect(0, 240 - 16, 16, 16, 128, 128, 128, 0xFF);
     g.setRect(320-16, 240 - 16, 16, 16, 128, 128, 0, 0xFF);
     
