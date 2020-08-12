88a957528cabc66eacb479df10303edecdf9601b
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-11 22:51:05 -0500

    getting started.

--- BadBits.Engine/Program.cs
+++ BadBits.Engine/Program.cs
@@ -1,11 +1,8 @@
 ﻿using CommandLine;
 using Jint.CommonJS;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Resources;
-using gl = OpenTK.Graphics.OpenGL.GL;
 
 namespace BadBits.Engine
 {
@@ -27,13 +24,61 @@ namespace BadBits.Engine
     ///         onClose: ()=> void,
     ///         onInit: ()=> void}
     ///         
-    /// </summary>
-    /// 
 
-    class ArgumentOptions
-    {
-        [Option()]
-        public string Path { get; set; }
+
+    class Game : Microsoft.Xna.Framework.Game {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Graphics2D _graphics2D;
+        Engine.ScriptHost _host;
+        Jint.Engine _scriptEngine; 
+        Jint.Native.JsValue _exports;
+
+        dynamic _globals;
+
+        Game() : base() {
+            this._graphics = new GraphicsDeviceManager(this);
+
+            _host = new ScriptHost();
+            _scriptEngine = new Jint.Engine();
+            _globals = new { };
+            _scriptEngine.SetValue("globals", _globals);
+            _scriptEngine.SetValue("badBits", _host);
+
+            _exports = _scriptEngine.CommonJS().RunMain(Environment.GetCommandLineArgs()[0]);
+
+        }
+
+        protected override void Initialize()
+        {
+            _spriteBatch = new SpriteBatch(this.GraphicsDevice);
+            _graphics2D = new Graphics2D();
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (_host.ProcessFunction != null)
+            {
+                _host.ProcessFunction.Invoke(gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            if (_host.Render3dFunction != null)
+            {
+                _host.Render3dFunction.Invoke(gameTime.ElapsedGameTime.TotalSeconds);
+            }
+
+            if (_host.Render2dFunction != null)
+            {
+                _host.Render2dFunction.Invoke(gameTime.ElapsedGameTime.TotalSeconds, _graphics2D);
+            }
+            base.Draw(gameTime);
+        }
     }
 
     static class Program
@@ -47,73 +92,61 @@ namespace BadBits.Engine
         static void Main(string[] args)
         {
 
-            Engine.ScriptHost host = new Engine.ScriptHost();
-            var scriptEngine = new Jint.Engine();
-
-            dynamic globals= new { };
-
-            scriptEngine.SetValue("globals", globals);
-            scriptEngine.SetValue("badBits", host);
-
-            // Creates a new Jint instance and runs the myModule.js file in the program's
-            // current working directory.
-            Jint.Native.JsValue exports = scriptEngine.CommonJS().RunMain(args[0]);
-
-            Graphics2D gfx2d = default(Graphics2D);
           
-   
-            using (var window = new OpenTK.GameWindow(640, 480, OpenTK.Graphics.GraphicsMode.Default, 
-                "Bad Bits Engine", OpenTK.GameWindowFlags.Default, 
-                OpenTK.DisplayDevice.Default, 2,1, 
-                OpenTK.Graphics.GraphicsContextFlags.Default))
-            {
-
-                window.Load += (o, e) =>
-                {
-                    if (!window.Context.IsCurrent)
-                    {
-                        window.Context.MakeCurrent(window.WindowInfo);
-                    }
-
-                    gfx2d = new Graphics2D();
-
-                };
 
 
-                window.Bounds = new Rectangle { X = 0, Y = 0, Width = 640, Height = 480 };
-                window.RenderFrame += (o, e) =>
-                {
-                    gl.Viewport(new Rectangle(0, 0, window.Width, window.Height));
-                    gl.ClearColor(Color.Black);
-                    gl.Clear(OpenTK.Graphics.OpenGL.ClearBufferMask.ColorBufferBit 
-                        | OpenTK.Graphics.OpenGL.ClearBufferMask.DepthBufferBit);
-                    
+            //using (var window = new Microsoft.Xna.Framework.Game())
+            //{
+            //    var adapter = new Microsoft.Xna.Framework.Graphics.GraphicsAdapter();
+                
+                
 
-                    if (host.Render3dFunction != null)
-                    {
-                        host.Render3dFunction.Invoke(e.Time);
-                    }
+            //    window.Load += (o, e) =>
+            //    {
+            //        if (!window.Context.IsCurrent)
+            //        {
+            //            window.Context.MakeCurrent(window.WindowInfo);
+            //        }
 
-                    if (host.Render2dFunction != null)
-                    {
-                      
-                    
-                        host.Render2dFunction.Invoke(e.Time,gfx2d);
-                    }
+            //        gfx2d = new Graphics2D();
 
-                    window.SwapBuffers();
-                };
+            //    };
 
-                window.UpdateFrame += (o, e) =>
-                {
-                    if (host.ProcessFunction != null)
-                    {
-                        host.ProcessFunction.Invoke(e.Time);
-                    }
-                };
-                window.Run();
 
-            }
+            //    window.Bounds = new Rectangle { X = 0, Y = 0, Width = 640, Height = 480 };
+            //    window.RenderFrame += (o, e) =>
+            //    {
+            //        gl.Viewport(new Rectangle(0, 0, window.Width, window.Height));
+            //        gl.ClearColor(Color.Black);
+            //        gl.Clear(OpenTK.Graphics.OpenGL.ClearBufferMask.ColorBufferBit
+            //            | OpenTK.Graphics.OpenGL.ClearBufferMask.DepthBufferBit);
+
+
+            //        if (host.Render3dFunction != null)
+            //        {
+            //            host.Render3dFunction.Invoke(e.Time);
+            //        }
+
+            //        if (host.Render2dFunction != null)
+            //        {
+
+
+            //            host.Render2dFunction.Invoke(e.Time, gfx2d);
+            //        }
+
+            //        window.SwapBuffers();
+            //    };
+
+            //    window.UpdateFrame += (o, e) =>
+            //    {
+            //        if (host.ProcessFunction != null)
+            //        {
+            //            host.ProcessFunction.Invoke(e.Time);
+            //        }
+            //    };
+            //    window.Run();
+
+            //}
 
         }
     }
