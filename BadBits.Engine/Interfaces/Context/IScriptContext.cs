1da7aa7d1318b69389e83ac44dc872b0c1969b67
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-13 20:30:15 -0500

    start building out interfaces.

---
+++ BadBits.Engine/Interfaces/Context/IScriptContext.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadBits.Engine.Interfaces.Context
+{
+    public interface IScriptContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initAction"></param>
+        void setInit(Action initAction);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="render2dAction"></param>
+        void setRender2d(Action<double, IGraphicsContext2d> render2dAction);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="render3dAction"></param>
+        void setRender3d(Action<double, IGraphicsContext3d> render3dAction);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="processAction"></param>
+        void setProcess(Action<double> processAction);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        void loadTexture(string name, string path);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        void createTexture(string name, int width, int height);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textureName"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="r"></param>
+        /// <param name="g"></param>
+        /// <param name="b"></param>
+        /// <param name="a"></param>
+        void setPixel(string textureName, int x, int y, byte r, byte g, byte b, byte a);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textureName"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="r"></param>
+        /// <param name="g"></param>
+        /// <param name="b"></param>
+        void setPixel(string textureName, int x, int y, byte r, byte g, byte b);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textureName"></param>
+        void refreshTexture(string textureName);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        /// <param name="cols"></param>
+        /// <param name="rows"></param>
+        void loadSpriteSheet(string name, string path, int cols, int rows);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="textureName"></param>
+        /// <param name="cols"></param>
+        /// <param name="rows"></param>
+        void setSpriteSheet(string name, string textureName, int cols, int rows);
+
+    }
+}
--- BadBits.Engine/ScriptHost.cs
+++ BadBits.Engine/ScriptHost.cs
@@ -4,15 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Jint;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace BadBits.Engine
 {
     public class ScriptHost
     {
+        private GraphicsDevice _graphics;
+
         public Action<double,IGraphics2D> Render2dFunction { get; private set; }
         public Action<double> Render3dFunction { get; private set; }
         public Action<double> ProcessFunction { get; private set; }
         public Action InitFunction { get; private set; }
+
+        public Dictionary<string,Texture2D> Textures { get; private set; }
+
+        public ScriptHost(GraphicsDevice graphics) {
+            _graphics = graphics;
+
+            Textures = new Dictionary<string, Texture2D>();
+        }
 
         public void setInit(Action initFunc) {
             this.InitFunction = initFunc;
