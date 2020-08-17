98da8937f5695a236371c3c8766a4a39d96633f9
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-16 20:59:51 -0500

    hey we have a demo!

---
+++ Demo1/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+engine.setInit(function () {
+});
+engine.setRender2d(function (dt) {
+});
---
+++ Demo1/src/index.ts
@@ -0,0 +1,11 @@
+import {IBadBits} from './bad-bits';
+
+declare var engine: IBadBits;
+
+engine.setInit(()=>{
+
+});
+
+engine.setRender2d((dt:number)=>{
+    
+});
