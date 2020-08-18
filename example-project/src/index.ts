d7eac2fadffef5ab92a1268a8309006db228bd13
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-18 14:43:25 -0500

    it shoots bullets

--- example-project/src/index.ts
+++ example-project/src/index.ts
@@ -1,4 +1,4 @@
-import { IBadBits } from '../../Typescript/bad-bits';
+import { IBadBits, IInputState } from '../../Typescript/bad-bits';
 
 declare var engine: IBadBits;
 declare var __dirname: string;
@@ -37,6 +37,14 @@ let maxScrollPos = spriteInfo.filter(x=> x.name==='background')[0].srcRect[2]-32
 
 let shipY = 120;
 
+interface bullet{
+    x:number,
+    y:number,
+    life:number
+};
+
+let bullets:bullet[] = [];
+
 engine.setInit(()=>{
 
     spriteInfo.forEach(x=>{
@@ -46,6 +54,8 @@ engine.setInit(()=>{
     engine.logInfo('hello world');
 
 });
+
+let lastInputState:IInputState = {} as IInputState;
 
 engine.setProcess((dt)=>{
 
@@ -60,6 +70,23 @@ engine.setProcess((dt)=>{
     if(shipY <0){shipY = 0;}
     if(shipY >210){shipY = 210;}
 
+    if(!lastInputState.a && inputState.a){
+        bullets.push({
+            x:64,
+            y:shipY+8,
+            life:16
+        });
+    }
+
+    lastInputState = inputState;
+
+    for(let b = 0; b < bullets.length; b++){
+        bullets[b].life = bullets[b].life - dt;
+        bullets[b].x  = bullets[b].x +(dt*60*(bullets[b].life/24));
+    }
+
+    bullets = bullets.filter(b=>b.life >0);
+
 });
 
 engine.setRender2d((dt)=>{
@@ -69,4 +96,18 @@ engine.setRender2d((dt)=>{
 
     const ship = spriteInfo.filter(s=>s.name === 'ship')[0];
     engine.drawTexture('ship',ship.srcRect, [32,shipY,ship.srcRect[2], ship.srcRect[3]]);
+
+    const bulletImage =spriteInfo.filter(s=>s.name==='laser-right')[0];
+
+    bullets.forEach(b=>{
+
+        engine.drawTexture(bulletImage.name,
+            bulletImage.srcRect, [
+                b.x,b.y,
+                bulletImage.srcRect[2],
+                bulletImage.srcRect[3]
+            ]);
+
+    });
+
 })
