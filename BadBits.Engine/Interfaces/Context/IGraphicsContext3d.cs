714a5180b1baa29d1a07cb911f17cffbbf7f06be
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-20 19:41:15 -0500

    eh

--- BadBits.Engine/Interfaces/Context/IGraphicsContext3d.cs
+++ BadBits.Engine/Interfaces/Context/IGraphicsContext3d.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace BadBits.Engine.Interfaces.Context
 {
     public interface IGraphicsContext3d
     {
+        void CreateShadedObject(string name, List<VertexPositionColor> vertexData);
+        void CreateTexturedObject(string name, string textureName, List<VertexPositionTexture> vertexData);
+
+
     }
 }
