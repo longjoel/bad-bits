8cff2983e25075946e5707476a0e8c2efde2ae4b
Joel Longanecker <joel.longanecker@gmail.com>
2020-08-26 20:09:04 -0500

    do more resource management

--- BadBits.Engine.Next/Interfaces/Services/IResourceManager.cs
+++ BadBits.Engine.Next/Interfaces/Services/IResourceManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BadBits.Engine.Next.Models.Host.Sprite;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +9,18 @@ using System.Threading.Tasks;
 
 namespace BadBits.Engine.Next.Interfaces.Services
 {
-    public class IResourceManager
+    public interface IResourceManager
     {
         Dictionary<string, Models.Host.Texture> TextureCache { get; }
-        Dictionary<string, Models.Host.Sprite.Sprite> SpriteCache { get; }
+        Dictionary<string, Sprite> SpriteCache { get; }
+
+        void CreateTexture(string name, int width, int height);
+        void LoadTexture(string name, string path);
+        void SetPixel(string textureName, int x, int y, Color color);
+        void UpdateTextures();
+
+        void CreateSprite(string spriteName);
+        void LoadSprite(string spriteName, string path);
+        void AddSpriteFrame(string spriteName, SpriteFrame spriteFrame);
     }
 }
